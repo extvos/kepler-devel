3778f6c5efcf3b08f8386ea1a7ea4e81b5fcce49
Mingcai SHEN <archsh@gmail.com>
2023-06-30 18:06:01 +0800

    Updated.

--- demo/handlers.go
+++ demo/handlers.go
@@ -4,6 +4,7 @@ import (
 	"github.com/extvos/kepler/contrib/restlet"
 	"github.com/extvos/kepler/service"
 	"github.com/extvos/kepler/servlet"
+	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -12,7 +13,8 @@ func hello(ctx servlet.RequestContext) error {
 	if nil != ctx.Session() {
 		log.Debugln("hello:session:> ", ctx.Session())
 	}
-	return ctx.Ctx().SendString("Hello")
+	p := ctx.Ctx().Params("param")
+	return ctx.Ctx().SendString("Hello," + p)
 }
 
 func middleware(ctx servlet.RequestContext) error {
@@ -20,9 +22,17 @@ func middleware(ctx servlet.RequestContext) error {
 	return ctx.Next()
 }
 
+func soapHandler(ctx servlet.RequestContext) error {
+	log.Debugln("soapHandler:> ", ctx.Ctx().Method(), ctx.Ctx().Path(), string(ctx.Ctx().Request().Header.ContentType()))
+	log.Debugln("soapHandler:> ", string(ctx.Ctx().Request().Body()))
+	s := "<soapenv:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:iptv=\"iptv\">\n\n\t<soapenv:Header/>\n\t<soapenv:Body>\n\t   <iptv:ExecCmdResponse soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n\t         <Result xsi:type=\"xsd:int\">0</Result>\n\t         <ErrorDescription xsi:type=\"soapenc:string\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\">?</ErrorDescription>\n\t</iptv:ExecCmdResponse>\n\t</soapenv:Body>\n\n</soapenv:Envelope>"
+	return ctx.Ctx().Status(fiber.StatusOK).SendString(s)
+}
+
 func init() {
 	service.Use("/hello", middleware)
 	service.Get("/", hello)
-	service.Get("/hello", hello)
-	service.All("/student", restlet.NewHandler[Student]())
+	service.Get("/hello/:param?", hello)
+	service.All("/student/:id?", restlet.NewHandler[Student]())
+	service.All("/soap", soapHandler)
 }
--- demo/models.go
+++ demo/models.go
@@ -1,13 +1,73 @@
 package main
 
-import xql "github.com/archsh/go.xql"
+import (
+	"database/sql/driver"
+	"github.com/archsh/go.xql/dialects/postgres"
+	_ "github.com/lib/pq"
+	"time"
+)
 
-type Student struct {
-	Id   xql.BigSerial `xql:"pk"`
-	Name string        `xql:"type=varchar,size=64,unique,nullable=false"`
-	Age  int           `xql:"type=Integer,nullable=false,default=0"`
+type School struct {
+	Id          int                  `json:"id" xql:"type=serial,pk"`
+	Name        string               `json:"name" xql:"size=24,unique,index"`
+	Tags        postgres.StringArray `json:"tags" xql:"size=32,nullable"`
+	Description string               `json:"description"  xql:"name=desc,type=text,size=24,nullable=false,default=''"`
 }
 
-func (Student) TableName() string {
+func (c School) TableName() string {
+	return "schools"
+}
+
+type Character struct {
+	postgres.JSON
+	Attitude string
+	Height   int
+	Weight   int
+}
+
+//func (j Character) Declare(props xql.PropertySet) string {
+//    return "JSONB"
+//}
+
+func (j *Character) Scan(value interface{}) error {
+	return postgres.JsonbScan(j, value)
+}
+
+func (j Character) Value() (driver.Value, error) {
+	return postgres.JsonbValue(j)
+}
+
+type People struct {
+	Id          int        `json:"id" xql:"type=serial,pk"`
+	FullName    string     `json:"fullName" xql:"size=80,unique=true,index=true"`
+	FirstName   string     `json:"firstName" xql:"size=24,default=''"`
+	MiddleName  string     `json:"middleName" xql:"size=24,default=''"`
+	LastName    string     `json:"lastName" xql:"size=24,default=''"`
+	Region      string     `json:"region"  xql:"size=24,nullable=true"`
+	Age         int        `json:"age" xql:"check=(age>18)"`
+	SchoolId    int        `json:"schoolId"  xql:"type=integer,fk=schools.id,ondelete=CASCADE"`
+	Description string     `json:"description"  xql:"name=desc,type=text,size=24,default=''"`
+	Created     *time.Time `json:"created"  xql:"type=timestamp,default=Now()"`
+	Updated     *time.Time `json:"Updated"  xql:"type=timestamp,default=Now()"`
+}
+
+type Teacher struct {
+	People
+	Degree string `json:"degree" xql:"size=64,default=''"`
+}
+
+func (t Teacher) TableName() string {
+	return "teachers"
+}
+
+type Student struct {
+	People
+	Grade      string               `json:"grade" xql:"size=32,default=''"`
+	Attributes postgres.HSTORE      `json:"attributes" xql:"nullable"`
+	Scores     postgres.DoubleArray `json:"scores" xql:"nullable"`
+	Character  Character            `json:"character" xql:"nullable"`
+}
+
+func (c Student) TableName() string {
 	return "students"
 }
